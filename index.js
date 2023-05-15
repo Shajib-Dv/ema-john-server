dcbf57b4f1fc0fea8c9bf2087d88d939971589e9
Shajib-Dv <mdsojeeb242@gmail.com>
2023-05-15 15:33:03 +0600

    pagination query added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,9 +34,27 @@ async function run() {
     const productCollection = client.db("emaJohnDB").collection("products");
 
     app.get("/products", async (req, res) => {
-      const cursor = productCollection.find();
+      // console.log(req.query);
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = page * limit;
+
+      const cursor = productCollection.find().skip(skip).limit(limit);
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    app.post("/productsByIds", async (req, res) => {
+      const ids = req.body;
+      const objectIds = ids.map((id) => new ObjectId(id));
+      const query = { _id: { $in: objectIds } };
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/totalProducts", async (req, res) => {
+      const result = await productCollection.estimatedDocumentCount();
+      res.send({ totalProducts: result });
     });
 
     // Send a ping to confirm a successful connection
